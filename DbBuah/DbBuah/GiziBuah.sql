fd7991aedeb52bb2a585274699407e8b2eba1424
dysnasp <dysnasp@gmail.com>
2021-04-29 00:07:12 +0700

    PBO 2021

---
+++ DbBuah/DbBuah/GiziBuah.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[GiziBuah]
+(
+	[NamaBuah] NVARCHAR(50) NOT NULL PRIMARY KEY, 
+    [Karbohidrat] FLOAT NOT NULL, 
+    [Protein] FLOAT NOT NULL, 
+    [Lemak] FLOAT NOT NULL, 
+    [Kalori] FLOAT NOT NULL 
+)
